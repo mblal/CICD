f110a1f2584e98e1a71672636a2f49e1590bb115
Mohamed BLAL <mohammed.blal@gmail.com>
2022-12-28 21:20:00 +0100

    Test

--- continious_integration.sh
+++ continious_integration.sh
@@ -1,1 +1,12 @@
-echo "Executed, Thanks!"
+#!/bin/bash
+
+#  Exit immediately if a command exits with a non-zero status
+set -eE
+
+export BEGIN_TIME=$(date +%s)
+export BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}i")" && pwd)"
+
+if [[ -z "${APP_NAME}" ]]
+	echo "Environment variable APP_NAME is not defined"
+	exit(1)
+fi
