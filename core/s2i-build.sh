e4a5c3c24dc9a35194926fd6e330ec4cf5642dfd
mblal <mohammed.blal@gmail.com>
2022-12-30 22:58:20 +0100

    Update s2i-build.sh

--- core/s2i-build.sh
+++ core/s2i-build.sh
@@ -2,5 +2,7 @@
 cp -R $APP_NAME "${APP_NAME}_TMP"
 cp -R conf/${APP_NAME}/.s2i "${APP_NAME}_TMP/"
 SOURCE_CODE="${APP_NAME}_TMP/"
+OUTPUT_IMAGE = "${APP_NAME}_im" | awk '{print tolower($0)}'
 echo ${DOCKER_IMAGE}
-sudo s2i build $SOURCE_CODE ${DOCKER_IMAGE} "${APP_NAME}-im"
+#sudo s2i build $SOURCE_CODE ${DOCKER_IMAGE} ${OUTPUT_IMAGE}
+sudo s2i build -c ${SOURCE_CODE} ${DOCKER_IMAGE} ${OUTPUT_IMAGE} --exclude "(^|/)\.git|.bindingroot(/|$)"
