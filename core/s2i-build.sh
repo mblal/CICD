c3cc926e35f10c28af2df612a5363591071f3199
mblal <mohammed.blal@gmail.com>
2022-12-30 22:11:50 +0100

    Update s2i-build.sh

--- core/s2i-build.sh
+++ core/s2i-build.sh
@@ -2,4 +2,5 @@
 cp -R $APP_NAME "${APP_NAME}_TMP"
 cp -R conf/${APP_NAME}/.s2i "${APP_NAME}_TMP/"
 SOURCE_CODE="${APP_NAME}_TMP/"
-s2i build $SOURCE_CODE ${DOCKER_IMAGE} "${APP_NAME}-im"
+echo ${DOCKER_IMAGE}
+sudo s2i build $SOURCE_CODE ${DOCKER_IMAGE} "${APP_NAME}-im"
