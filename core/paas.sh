2e888ae9d842d9be81b7adefe3637631ea41fcef
Mohamed BLAL <mohammed.blal@gmail.com>
2023-01-01 21:08:34 +0100

    Instruct k8s deploy

--- core/paas.sh
+++ core/paas.sh
@@ -2,6 +2,8 @@ export OUTPUT_IMAGE=`echo "${APP_NAME}_im"|awk '{print tolower($0)}'`
 DOCKER_IMAGE_NORMALIZED_NAME=`echo "${OUTPUT_IMAGE}" | tr '[:upper:]' '[:lower:]' | sed 's/_/-/g'`
 export DOCKER_IMAGE_NORMALIZED_NAME=`echo "${DOCKER_IMAGE_NORMALIZED_NAME}" | tr '[:upper:]' '[:lower:]' | sed 's/[^a-z0-9.-]//g'`
 
+export NORMALIZED_APP_NAME=`echo "${APP_NAME}" | tr '[:upper:]' '[:lower:]' | sed 's/_/-/g'`
+
 case "$1" in
 	"build")
 	      action $1
