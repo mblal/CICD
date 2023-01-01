f82cec83524c763b6d32a40068abc554de18ee5a
Mohamed BLAL <mohammed.blal@gmail.com>
2023-01-01 21:00:27 +0100

    Instruct k8s deploy

--- core/k8s-deploy.sh
+++ core/k8s-deploy.sh
@@ -6,4 +6,7 @@ echo $DOCKER_IMAGE_NORMALIZED_NAME;
 echo $DOCKER_REPO_URL
 template="${BASEDIR}/../conf/${APP_NAME}/template.yaml"
 envsubst <"${template}" >"${template}.tmp"
+
+echo "-------------------------------------------------------------"
 kubectl create -f "${BASEDIR}/../conf/${APP_NAME}/template.yaml.tmp"
+echo "-------------------------------------------------------------"
