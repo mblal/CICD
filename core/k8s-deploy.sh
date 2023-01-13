83ebd0d8541503a61f6fcc1f8243c3d3ee7671f5
mblal <mohammed.blal@gmail.com>
2023-01-13 22:47:46 +0100

    Update k8s-deploy.sh

--- core/k8s-deploy.sh
+++ core/k8s-deploy.sh
@@ -7,8 +7,8 @@ echo $DOCKER_REPO_URL
 template="${BASEDIR}/../conf/${APP_NAME}/template.yaml"
 envsubst <"${template}" >"${template}.tmp"
 
-sKINDS="$(yq '.kind' template.yaml.tmp | tr --delete '\n' | sed 's/---/,/g')"
-sOBJECT_NAMES="$(yq '.metadata.name' template.yaml.tmp | tr --delete '\n' | sed 's/---/,/g')"
+sKINDS="$(yq '.kind' "${template}.tmp" | tr --delete '\n' | sed 's/---/,/g')"
+sOBJECT_NAMES="$(yq '.metadata.name' "${template}.tmp" | tr --delete '\n' | sed 's/---/,/g')"
 
 IFS=',' read -ra KINDS <<< "$sKINDS"
 IFS=',' read -ra NAMES <<< "$sOBJECT_NAMES"
